91971cda973e48adb91c1c4cba1c194846f61dd5
yaowaluk261 <124548040+yaowaluk261@users.noreply.github.com>
2023-04-28 16:34:25 +0700

    Add files via upload

---
+++ tourpoint.js
@@ -0,0 +1,155 @@
+{
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "เอเชียทีค",
+          "type": "special",
+          "level": 6
+        },
+        "geometry": {
+          "coordinates": [
+            100.50414500000073,
+            13.705033262253963
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "ซาฟารีเวิลด์",
+          "type": "natural",
+          "level": 6
+        },
+        "geometry": {
+          "coordinates": [
+            100.70411124999981,
+            13.866687081581162
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "ฟาร์มจระเข้",
+          "type": "natural",
+          "level": 5
+        },
+        "geometry": {
+          "coordinates": [
+            100.59773950000277,
+            13.571918272833628
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "วัดพระธาตุดอยสุเทพ",
+          "type": "culture",
+          "level": 7
+        },
+        "geometry": {
+          "coordinates": [
+            98.92138149999818,
+            18.805010093532033
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "เกาะล้าน",
+          "type": "natural",
+          "level": 8
+        },
+        "geometry": {
+          "coordinates": [
+            100.78918799999894,
+            12.92606336576182
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "ภูลมโล",
+          "type": "natural",
+          "level": 6
+        },
+        "geometry": {
+          "coordinates": [
+            101.07581649999878,
+            16.98528770886159
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "น้ำตกห้วยแม่ขมิ้น",
+          "type": "natural",
+          "level": 7
+        },
+        "geometry": {
+          "coordinates": [
+            98.98505450000135,
+            14.639943543583286
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "แม่กำปอง",
+          "type": "special",
+          "level": 5
+        },
+        "geometry": {
+          "coordinates": [
+            99.34192300000484,
+            18.864526817131747
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "เกาะพีพี",
+          "type": "natural",
+          "level": 10
+        },
+        "geometry": {
+          "coordinates": [
+            98.7662785000021,
+            7.684359006187606
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "สวนสยาม",
+          "type": "special",
+          "level": 7
+        },
+        "geometry": {
+          "coordinates": [
+            100.69524850000028,
+            13.806196335280305
+          ],
+          "type": "Point"
+        }
+      }
+    ]
+  }
